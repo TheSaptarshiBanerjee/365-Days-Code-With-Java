afa02c8d95246609136c9b9566671d8ca37ddd76
Saptarshi Banerjee <90561422+TheSaptarshiBanerjee@users.noreply.github.com>
2023-08-22 20:53:36 +0530

    find the name in given array

---
+++ Linear Search/findName.java
@@ -0,0 +1,34 @@
+/**
+ * test
+ */
+import java.util.*;
+public class findName {
+    public static int find(String name[], String key)
+    {
+        for (int i = 0; i < name.length; i++) {
+            if (name[i].equalsIgnoreCase(key)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String name[] = {"Saptarshi","Rick","Rintu","Shubhajit"};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your desire searching name :");
+
+        String key = sc.nextLine();
+
+        sc.close();
+        int index = find(name, key);
+        if (index == -1) {
+            System.out.println("Not found");
+        }else
+        {
+            System.out.println("Name found at index : "+ index);
+        }
+        
+       
+    }
+}
