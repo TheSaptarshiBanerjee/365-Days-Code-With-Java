057a4dcd79e3dacf34fbbc1fca8bb46043ddb48c
Saptarshi Banerjee <90561422+TheSaptarshiBanerjee@users.noreply.github.com>
2023-08-23 23:44:54 +0530

    Day 2 of 365 days

---
+++ FindLargest.java
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                         Find largest number in the given Array
+
+*******************************************************************************/
+import java.util.*;
+
+
+public class FindLArgest
+{
+    public static int largest(int nums[])
+{
+    int largest = Integer.MIN_VALUE;
+    
+    for (int i = 0;i<nums.length ;i++ )
+    {
+    if (largest<nums[i])
+    {
+        largest = nums[i];
+    }
+    }
+    return largest;
+}
+	public static void main(String[] args) {
+		int nums[] = {4,2,8,10,5};
+		System.out.println("THE LARGEST NUMBER : "+largest(nums));
+	}
+}
